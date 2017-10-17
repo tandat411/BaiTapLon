f415d2febc83259867fda4b234b1882c50dfb79a
KhanhHoang <1551010037hoang@ou.edu.vn>
2017-10-17 20:45:41 +0700

    Hoan thanh xem diem thi

--- ChucNang.cs
+++ ChucNang.cs
@@ -21,6 +21,7 @@ namespace QLSV
         {
             if (MessageBox.Show("Bạn muốn thoát chương trình ?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.No)
                 e.Cancel = true;
+            DangNhap.cn.Close();
         }
 
         private void ChucNang_Load(object sender, EventArgs e)
@@ -67,6 +68,7 @@ namespace QLSV
             SqlCommand cmd8 = new SqlCommand(sqlNienKhoa, DangNhap.cn);
             cmd8.CommandType = CommandType.Text;
             txtNienKhoa.Text = Convert.ToString(cmd8.ExecuteScalar());
+            DangNhap.cn.Close();
         }
 
         private void btXemDiem_Click(object sender, EventArgs e)
--- Xemdiem.cs
+++ Xemdiem.cs
@@ -6,7 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 namespace QLSV
 {
     public partial class Xemdiem : Form
@@ -18,7 +18,84 @@ namespace QLSV
 
         private void Xemdiem_Load(object sender, EventArgs e)
         {
-
+            DangNhap.cn.Open();
+            //ho
+            txtMSSV.Text = DangNhap.mssv;
+            string sqlHo = "SELECT Ho FROM SinhVien WHERE MaSinhVien ='" + DangNhap.mssv + "'";
+            SqlCommand cmd1 = new SqlCommand(sqlHo, DangNhap.cn);
+            string ho = (string)cmd1.ExecuteScalar();
+            //ten
+            string sqlTen = "SELECT Ten FROM SinhVien WHERE MaSinhVien ='" + DangNhap.mssv + "'";
+            SqlCommand cmd2 = new SqlCommand(sqlTen, DangNhap.cn);
+            cmd2.CommandType = CommandType.Text;
+            string ten = (string)cmd2.ExecuteScalar();
+            txtHoten.Text = ho + " " + ten;
+            //Khoa
+            string sqlKhoa = "SELECT  TenKhoa FROM SinhVien,Khoa WHERE MaSinhVien ='" + DangNhap.mssv + "' and SinhVien.MaKhoa = Khoa.MaKhoa";
+            SqlCommand cmd3 = new SqlCommand(sqlKhoa, DangNhap.cn);
+            cmd3.CommandType = CommandType.Text;
+            txtKhoa.Text = Convert.ToString(cmd3.ExecuteScalar());
+            ///
+            string sql= "SELECT * FROM SinhVien WHERE MaSinhVien ='" + txtMSSV.Text+"'";
+            SqlCommand cmd4 = new SqlCommand(sql, DangNhap.cn);
+            cmd4.CommandType = CommandType.Text;
+            dlgXemdiem.DataSource = GetData();
         }
+        private List<object> GetData()
+        {
+            //DangNhap.cn.Open();
+
+            string sql = "SELECT * FROM Thi WHERE MaSinhVien ='" + DangNhap.mssv + "'";
+
+            List<object> list = new List<object>();
+            try
+            {
+                SqlCommand cmd = new SqlCommand(sql, DangNhap.cn);
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                string maLop , maSinhVien, phongThi , caThi;
+                double diemthiGK;
+                double diemthiCK;
+               // int c = Convert.ToInt32("diemthiCK");
+                int lanthi;
+                DateTime ngayThi;
+                while (dr.Read())
+                {
+                    maLop = dr.GetString(0);
+                    maSinhVien = dr.GetString(1);
+                    lanthi = dr.GetInt32(2);
+                   ngayThi = dr.GetDateTime(3);
+                    phongThi = dr.GetString(4);
+                    caThi = dr.GetString(5);
+                    diemthiGK = dr.GetDouble(6);
+                    diemthiCK = dr.GetDouble(7);
+                    // ...
+
+                    var prod = new
+                    {
+                        MaLop = maLop,
+                        MSSV = maSinhVien,
+                        LanThi = lanthi,
+                       NgayThi = ngayThi,
+                        PhongThi=phongThi,
+                        Ca=caThi,
+                       DiemThiGK=diemthiGK,
+                       DiemThiCK=diemthiCK,
+                    };
+                    list.Add(prod);
+                }
+                dr.Close();
+            }
+            catch (SqlException ex) // Internet => Exception
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                DangNhap.cn.Close();
+            }
+            return list;
+        }
+
     }
 }
